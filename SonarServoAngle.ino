792d25e693adbc153ecf6f2bd82ed11443ec5dfe
pieterVL <pieter.van.leuven01@gmail.com>
2017-04-26 14:50:55 +0200

    basic servo hand tracking hardcoded 5.5cm

--- SonarServoAngle.ino
+++ SonarServoAngle.ino
@@ -1,16 +1,28 @@
 #include <NewPing.h>
+#include <Servo.h>
 #define TRIGGER_PIN  12
 #define ECHO_PIN     11
 #define MAX_DISTANCE 20
  
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-void setup() { 
+Servo myservo;
+void setup() {
   Serial.begin(9600);
+   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
  
 void loop() {
-  delay(50);
-  Serial.print("Ping: ");
+  delay(200);
+  /*Serial.print("Ping: ");
   Serial.print(sonar.ping_cm());
-  Serial.println("cm");
+  Serial.println("cm");*/
+  int sonarval = sonar.ping_cm();
+  int val = map(sonarval, 1,16,50,130);
+  Serial.print(sonarval);
+  Serial.print("   ");
+  Serial.println(val);
+  if(sonarval > 0){
+    //int val = map(sonarval, 1,16,50,130);
+    myservo.write(val);//50-130
+  }
 }
